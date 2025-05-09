2242debe78eedcb5cca32c56d930c83af6ea6e2b
JonyM355 <joaoricardoam05@gmail.com>
2025-05-09 14:22:58 +0100

    Doing class Player

---
+++ EqualPlayer/Player.cs
@@ -0,0 +1,19 @@
+namespace EqualPlayer
+{
+    public class Player
+    {
+        public PlayerClass PClass { get; }
+        public string Name { get; }
+
+        public Player(PlayerClass pClass, string name)
+        {
+            PClass = pClass;
+            Name = name;
+        }
+
+        public override int GetHashCode()
+        {
+            return PClass.GetHashCode() ^ Name.GetHashCode();
+        }
+    }
+}
