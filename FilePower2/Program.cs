60aee8285e7fb87057325f0f0a7e7f3b7abeed29
JonyM355 <joaoricardoam05@gmail.com>
2025-05-09 16:31:45 +0100

    Doing project FilePower2

---
+++ FilePower2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilePower2
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {   
+            string fileName = args[0];
+
+            Console.WriteLine("Insert phrases:");
+
+            {
+                string line;
+
+                using StreamWriter sw = new StreamWriter(fileName);
+
+                while (true)
+                {
+                    line = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        break;
+                    }
+                    
+                    sw.WriteLine(line);
+                }
+            }
+            Console.WriteLine("Frases guardadas com sucesso!");
+        }
+    }
+}
