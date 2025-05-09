03c9c36b4a0b929a6523147a4c466ca482eca45c
JonyM355 <joaoricardoam05@gmail.com>
2025-05-09 14:23:17 +0100

    Doing project EqualPlayer

---
+++ EqualPlayer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualPlayer
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            HashSet<Player> setOfPlayers = new HashSet<Player>
+            {
+                new Player(PlayerClass.Tank, "Ana"),
+                new Player(PlayerClass.Slayer, "Paulo"),
+                new Player(PlayerClass.Tank, "Ana") // Duplicado
+            };
+
+            foreach (Player p in setOfPlayers)
+            {
+                Console.WriteLine($"{p.Name} is a {p.PClass}");
+            }
+        }
+    }
+}
